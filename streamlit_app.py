78ae9e61130be20db27b76bfeb1c5103b68cde5a
AndyDennisonGit <69978925+AndyDennisonGit@users.noreply.github.com>
2026-09-22 06:16:41 +0100

    Update streamlit_app.py
    fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if ingredients_list:  # is not null
         st.success(name_on_order_message, icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
