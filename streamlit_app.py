573a38c0df072ead4ff45440c3d4e8560094612d
AndyDennisonGit <69978925+AndyDennisonGit@users.noreply.github.com>
2026-09-22 06:24:27 +0100

    Update streamlit_app.py
    Added a data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,5 @@ if ingredients_list:  # is not null
         session.sql(my_insert_stmt).collect()
         st.success(name_on_order_message, icon="✅")
 
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
